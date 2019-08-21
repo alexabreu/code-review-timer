f94df76a382da7736236d9057a3a506b7228a1d0
Alex Abreu <alexabreu@gmail.com>
2019-08-20 22:02:54 -0400

    remove global state

--- src/index.ts
+++ src/index.ts
@@ -6,17 +6,6 @@ import configDefaults from './config'
 
 type PullRequest = Octokit.PullsGetResponse
 
-interface StateInterface {
-  labels: Label[];
-}
-
-const state: StateInterface = {
-  labels: [],
-}
-
-let config: Config
-let scheduler: any;
-
 const removeExistingLabelsFromPullRequest = 
   async(  context: Context, 
           pullRequest: PullRequest, 
@@ -26,7 +15,7 @@ const removeExistingLabelsFromPullRequest =
 
   context.log('Removing exisiting labels from pull request', pullRequest.number, 'shouldRemoveAll', shouldRemoveAll)
   const issueParams = context.issue()
-  const labelToBeAdded = getLabelToBeAddedToPullRequest(pullRequest)
+  const labelToBeAdded = await getLabelToBeAddedToPullRequest(context, pullRequest)
   const existingLabels = pullRequest.labels
   const labels = labelToBeAdded && !shouldRemoveAll ?
     existingLabels.filter(l => l.name !== labelToBeAdded.name) : 
@@ -44,17 +33,16 @@ const removeExistingLabelsFromPullRequest =
   }
 }
 
-const getLabelToBeAddedToPullRequest = (pullRequest: PullRequest) => {
-  const { codeReviewTimeWindow, reminderCount } = config
+const getLabelToBeAddedToPullRequest = async (context: Context, pullRequest: PullRequest) => {
+  const { codeReviewTimeWindow, reminderCount, emoji, labelColor: color } = await loadConfig(context)
   const timeIncrement = codeReviewTimeWindow / reminderCount
   const now = Moment()
   const elapsedTime = Moment(now).diff(pullRequest.created_at, 'milliseconds')
 
   for (let i = reminderCount; i >= 1; i--) {
     if (elapsedTime >= timeIncrement * i) {
-      const label = state.labels.find(l => l.name.split(' ').length >= i)
-
-      if (label) return label
+      const label = getLabel(emoji, color, i)
+      return label
     }
   }
 
@@ -68,7 +56,7 @@ const pullRequestAlreadyHasLabel = (pullRequest: PullRequest, label: Label) => {
 
 const addLabelToPullRequest = async (context: Context, pullRequest: PullRequest) => {
   const issueParams = context.issue()
-  const label = getLabelToBeAddedToPullRequest(pullRequest)
+  const label = await getLabelToBeAddedToPullRequest(context, pullRequest)
 
   if (!label) return
   if (pullRequestAlreadyHasLabel(pullRequest, label)) return
@@ -77,20 +65,28 @@ const addLabelToPullRequest = async (context: Context, pullRequest: PullRequest)
   return context.github.issues.addLabels({ ...issueParams, issue_number: pullRequest.number, labels: [label.name] })  
 }
 
-const createLabels = async (context: Context) => {
-  const { emoji, labelColor: color, reminderCount } = config
+const getLabel = (emoji: string, color: string, length: number) => {
   let label = ''
-  for (let i = 1; i <= reminderCount; i++) {
+  for (let i = 1; i <= length; i++) {
     label += `${emoji} `
-    state.labels.push({
-      name: label.trim(),
-      color
-    })
+  }
+
+  return {
+    name: label.trim(),
+    color
+  }
+}
+
+const createLabels = async (context: Context) => {
+  const { emoji, labelColor: color, reminderCount } = await loadConfig(context)
+  const labels = []
+  for (let i = 1; i <= reminderCount; i++) {
+    labels.push(getLabel(emoji, color, i))
   }
 
   try {
     const issueParams = context.issue()
-    for (let l of state.labels) {
+    for (let l of labels) {
       await context.github.issues.createLabel({...issueParams, ...l})
     }
   } catch (e) {
@@ -104,7 +100,7 @@ const runOnAllPullRequests =
           state: 'open' | 'closed' | 'all' = 'open'
   ) => 
 {
-  const { shouldIgnoreDraftPullRequests } = config
+  const { shouldIgnoreDraftPullRequests } = await loadConfig(context)
   const pullRequestParams = context.issue()
   const pullRequestsResponse = await context.github.pulls.list({ ...pullRequestParams, state })
   if (!(pullRequestsResponse && pullRequestsResponse.data)) return
@@ -120,26 +116,27 @@ const runOnAllPullRequests =
 }
 
 const loadConfig = async (context: Context) => {
+  let config: Config
   try {
     config = await context.config('code-review-timer.yml') as Config
+    if (!config) config = { ...configDefaults }
   } catch(e) {
     context.log('No configuration found. Using defaults.', e)
     config = { ...configDefaults }
-  } finally {
-    context.log('Configuration', config)
-  } 
+  }
+
+  context.log('Configuration', config)
+  return config
 }
 
 export = (app: Application) => {  
   app.on('schedule.repository', async (context) => {
-    await loadConfig(context)
     await runOnAllPullRequests(context, removeExistingLabelsFromPullRequest)
     await runOnAllPullRequests(context, addLabelToPullRequest)
   })
 
   app.on('pull_request.closed', async (context) => {
     context.log('Pull request closed...')
-    await loadConfig(context)
     await runOnAllPullRequests(
       context,
       (context: Context, pullRequest: PullRequest) => removeExistingLabelsFromPullRequest(context, pullRequest, true),
@@ -148,8 +145,8 @@ export = (app: Application) => {
   })
 
   app.on(['pull_request.opened', 'pull_request.reopened'], async(context) => {
-    await loadConfig(context)
-    createScheduler(app, { delay: true, interval: config.pollIntervalTime })
+    let { pollIntervalTime: interval } = await loadConfig(context)
+    createScheduler(app, { delay: true, interval })
     createLabels(context)
   });
 

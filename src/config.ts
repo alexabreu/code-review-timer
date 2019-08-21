fefd9a2e19a4186a5494a4b159057561d6e4346b
Alex Abreu <alexabreu@gmail.com>
2019-08-21 09:25:53 -0400

    defaults

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,11 @@
+// GitHub labels must be <= 50 chars. 
+// Keep this in mind when picking an emoji and reminderCount.
+// The combination of the two could result in longer labels.
 const configDefaults: Config = {
   emoji: ":alarm_clock:",
   labelColor: "c5def5",
   codeReviewTimeWindow: 48 * 60 * 60 * 1000,
-  reminderCount: 5,
+  reminderCount: 3, 
   pollIntervalTime: 0.5 * 60 * 60 * 1000,
   shouldIgnoreDraftPullRequests: true,
 }
